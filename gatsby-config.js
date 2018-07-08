3e2b87ccda4e677bf99ae943789d4a1f0ff4fe23
Justin W Hall <justinwhall@me.com>
2018-07-08 13:40:23 -0600

    add ga

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,6 +43,12 @@ module.exports = {
         ],
       }
     },
+    // {
+    //   resolve: 'gatsby-plugin-google-analytics',
+    //   options: {
+    //     trackingId: config.googleAnalyticsID,
+    //   },
+    // },
     {
       resolve: 'gatsby-plugin-nprogress',
       options: {
